37e1972272549609c1aaa8da6ec1d5612b2fc2f0
Aleksey Romanov <badkidddd@gmail.com>
2021-12-13 17:54:38 +0300

    hw-5

--- src/routes/User/index.tsx
+++ src/routes/User/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {SWITCH_CHECKBOX} from "../../store/user/actions";
 
 export const User = () => {
+    const dispatch = useDispatch();
+    const checkbox = useSelector<any>((state) => state.checkbox) as boolean;
+
+    const onChangeInput = (event: any) => {
+        dispatch({type: SWITCH_CHECKBOX, payload: event.target.checked})
+    }
+
     return (
         <div>
             <h1>User</h1>
+            <input type='checkbox' checked={checkbox} onChange={onChangeInput}/>
         </div>
     )
 }
